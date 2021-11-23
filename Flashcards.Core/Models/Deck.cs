b69df069867c941ca53f78f62bbeab76655bde14
kamila0801 <kami7686@easv365.dk>
2021-11-23 10:28:02 +0100

    models

---
+++ Flashcards.Core/Models/Deck.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Flashcards_backend.Core.Models
+{
+    public class Deck
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public User User { get; set; }
+        public List<Card> Cards { get; set; }
+        public bool isPublic { get; set; }
+
+    }
+}
