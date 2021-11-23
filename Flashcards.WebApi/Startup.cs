ffb240f10c24e457cbeb3ded63cfe25ce8212752
kubaczechowski <jcobczechowski@gmail.com>
2021-11-23 20:25:26 +0100

    update for create deck functionality

--- Flashcards.WebApi/Startup.cs
+++ Flashcards.WebApi/Startup.cs
@@ -105,7 +105,7 @@ namespace Flashcards.WebApi
         }
 
 
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SecurityContext securityContext,
             MainDbContext context)
